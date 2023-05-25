728652f277181d65655161844a6e32c6e25d4275
Heming Zhen <Hotrank@users.noreply.github.com>
2023-05-25 17:49:45 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -l
 set -euo pipefail
 
-echo $(pwd)
-echo $(ls)
-cd .github
-echo $(pwd)
+# echo $(pwd)
+# echo $(ls)
+# cd .github
+# echo $(pwd)
 # Set the default path to the script
 SCRIPT_FILEPATH=${SCRIPT_FILEPATH:=".github/sendgrid.js"}
 
 
-echo $(ls)
+# echo $(ls)
 
 # Execute the script
 $SCRIPT_FILEPATH
